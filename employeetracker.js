49f3494eaba2864cd245a9604cc78f2a1c2d8756
Prince of Kochi <mathenjs@gmail.com>
2020-07-21 17:57:47 -0700

    working initial inquirer prompt

--- employeetracker.js
+++ employeetracker.js
@@ -1,6 +1,6 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
-var cTable = requirer("console.table");
+var cTable = require("console.table");
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -13,17 +13,25 @@ var connection = mysql.createConnection({
 connection.connect(function(err){
     if (err) throw err;
     console.log("connected as id" + connection.threadID + "\n");
+    start();
 })
 
-inquirer
-    .prompt([
-        {
-            type: "list",
-            message: "What would you like to do?",
-            choices: ["View All Employees", "View All Employees By Department", "View All Employees By Manager", "Add Employee","Remove Employee", "Update Employee Role", "Update Employee Manager"],
-            name: "choice"
-    }
+function start() {
+    console.log("inside start");
+    inquirer
+    .prompt({
+      name: "action",   
+      type: "rawlist",
+      message: "What would you like to do?",
+      choices: [
+        "View All Employees", 
+        "View All Employees By Department", 
+        "View All Employees By Manager", 
+        "Add Employee","Remove Employee", 
+        "Update Employee Role", 
+        "Update Employee Manager"
+      ]
+    });
+}
 
-])
-    
     
