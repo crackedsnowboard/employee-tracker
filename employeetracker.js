59026c38dced051ba6605fa8e9fa4c835ad6c0e5
Prince of Kochi <mathenjs@gmail.com>
2020-07-21 18:17:03 -0700

    switch case added to start function

--- employeetracker.js
+++ employeetracker.js
@@ -4,13 +4,13 @@ var cTable = require("console.table");
 
 var connection = mysql.createConnection({
     host: "localhost",
-    port:3306,
+    port: 3306,
     user: "root",
     password: "password",
     database: "employee_trackerDB"
 });
 
-connection.connect(function(err){
+connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id" + connection.threadID + "\n");
     start();
@@ -19,19 +19,44 @@ connection.connect(function(err){
 function start() {
     console.log("inside start");
     inquirer
-    .prompt({
-      name: "action",   
-      type: "rawlist",
-      message: "What would you like to do?",
-      choices: [
-        "View All Employees", 
-        "View All Employees By Department", 
-        "View All Employees By Manager", 
-        "Add Employee","Remove Employee", 
-        "Update Employee Role", 
-        "Update Employee Manager"
-      ]
-    });
+        .prompt({
+            name: "action",
+            type: "rawlist",
+            message: "What would you like to do?",
+            choices: [
+                "View All Employees",
+                "View All Employees By Department",
+                "View All Employees By Manager",
+                "Add Employee",
+                "Remove Employee",
+                "Update Employee Role",
+                "Update Employee Manager"
+            ]
+        })
+        .then(function (answer) {
+            switch (answer.action) {
+                case "View All Employees":
+                    viewEmployees();
+                    break;
+                case "View All Employees By Department":
+                    viewEmployeesDepartment();
+                    break;
+                case "View All Employees By Manager":
+                    viewEmployeesManager();
+                    break;
+                case "Add Employee":
+                    addEmployee();
+                    break;
+                case "Remove Employee":
+                    removeEmployee();
+                    break;
+                case "Update Employee Role":
+                    updateEmployeeRole();
+                    break;
+                case "Update Employee Manager":
+                    updateEmployeeManager();
+                    break;
+            }
+        });
 }
 
-    
